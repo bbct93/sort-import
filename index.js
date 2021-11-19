36f5a3e0a940cef5ad9cf6c03649265ef848ebf8
chenTao <bbct1993>
2021-11-19 17:53:12 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,36 @@
+const parse = require("@babel/parser").parse;
+const generate = require("@babel/generator").default;
+
+let code = `
+import fn from './a/b';
+import { Select,  Pagination } from '@babel/parse';
+import { a } from '@/babel/parse';
+import { x } from '/babel/parse';
+export default {
+
+    data() {
+    return {
+        a: 1
+    }
+    }
+}
+`;
+// parse 解析
+const ast = parse(code, { sourceType: "module" });
+// 所有import节点
+const importDeclarationList = ast.program.body.filter(item => item.type === 'ImportDeclaration');
+const firstList = importDeclarationList.filter(item => /^@[a-zA-Z]/.test(item.source.value))
+const secondList = importDeclarationList.filter(item => /^@\//.test(item.source.value))
+const thirdLit = importDeclarationList.filter(item => /^\//.test(item.source.value))
+const lastList = importDeclarationList.filter(item => /^\./.test(item.source.value))
+
+const rightList = [...firstList, ... secondList, ... thirdLit, ...lastList];
+ast.program.body = ast.program.body.filter(item => item.type !== 'ImportDeclaration');
+ast.program.body = [...rightList, ...ast.program.body];
+let newCode = generate(ast).code;
+console.log('newCode---->',newCode);
+
+// ImportDeclaration
+
+
+
